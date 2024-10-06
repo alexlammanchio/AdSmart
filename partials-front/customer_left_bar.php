72fc93c68722f57b8353a2b4d476bc5533651741
alexlammanchio <161237834+alexlammanchio@users.noreply.github.com>
2024-10-06 23:46:55 +0800

    Add files via upload

---
+++ partials-front/customer_left_bar.php
@@ -0,0 +1,44 @@
+<div class="left-menu" >  
+                        <ul id="navbarNav">  
+                            <li >  
+                                <a href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=1">Home </a>  
+                            </li> 
+                            <li >  
+                                <a href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=6">Account Profile </a>  
+                            </li> 
+                            <li>
+            	            <a href="#" class="desktop-link1" style="display: none;">Quotation</a>
+            	            <input type="checkbox" id="show-features1" style=" opacity: 0;margin: -5px;">
+            	            <label for="show-features1">Quotation</label>
+            	            <ul>
+            	              <li><a href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=2" >Create New Quotation</a></li>
+            				  <li><a href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=3" >Check Quotation Status</a></li>
+            	            </ul>
+            	          </li>
+                            <li>  
+                                <a  href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=4">Explore Recommandation Company</a>  
+                            </li>  
+                             <li>  
+                                <a href="#" class="desktop-link1" style="display: none;">Order Status</a>
+            	            <input type="checkbox" id="show-features1" style=" opacity: 0;margin: -5px;">
+            	            <label for="show-features1">Order Status</label>
+            	            <ul>
+            	            <li><a href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=8" > Checkout the Items</a></li>
+            	              <li><a href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=7" >Check in-progress order status</a></li>
+            				  <li><a href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=5" >Check Purchase history</a></li>
+            	            </ul>  
+                            </li> 
+                            <li>  
+                                <a href="#" class="desktop-link1" style="display: none;">Report</a>
+            	            <input type="checkbox" id="show-features1" style=" opacity: 0;margin: -5px;">
+            	            <label for="show-features1">Report</label>
+            	            <ul>
+            	            <li><a href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=9" > Transaction Report</a></li>
+            	              <li><a href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=10" >Spend Report</a></li>
+            				  <li><a href="<?php  echo USER_MANAGEMENT;?>Adsmart_customers_personal_space.php?page=11" >Purchase Product Type Report</a></li>
+            	            </ul>  
+                            </li> 
+                        </ul>  
+             </div> 
+
+	    <!-- Menu Section Ends -->
